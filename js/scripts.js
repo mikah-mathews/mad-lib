817d1b6920fb8dead87d46f88811471651b7d802
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-03-25 09:43:39 -0700

    add javascript for form updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
-  $("#form-one").submit(function() {
-    $(".person1").append("blah blah");
-    $(".place1").append("blah blah");
-    $(".monster").append("blah blah");
-    $(".scary-noise").append("blah blah");
-    $(".exclamation").append("blah blah blah");
-    $(".trap").append("blah blah blah");
-    $(".villain").append("blah blah");
+  $("#form-one").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var place1Input = $("input#place1").val();
+    var monsterInput = $("input#monster").val();
+    var scaryNoiseInput = $("input#scary-noise").val();
+    var exclamationInput = $("input#exclamation").val();
+    var trapInput = $("input#trap").val();
+    var villainInput = $("input#villain").val();
+
+
+    $(".person1").text(person1Input);
+    $(".place1").text(place1Input);
+    $(".monster").text(monsterInput);
+    $(".scary-noise").text(scaryNoiseInput);
+    $(".exclamation").text(exclamationInput);
+    $(".trap").text(trapInput);
+    $(".villain").text(villainInput);
+
+    $("#story").show();
+
+    event.preventDefault();
   });
 });
